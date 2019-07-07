30de96b8b200a9da7aac96aef7236127d83dd15f
Alexandr Piskarev <unscarred@mail.ru>
2019-07-07 14:28:44 +0300

    Added solution for task17
    Added new module for Mantis application tests

---
+++ mantis-tests/src/test/java/mantis/tests/TestBase.java
@@ -0,0 +1,26 @@
+package mantis.tests;
+
+import mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.IOException;
+
+
+public class TestBase {
+
+    static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws IOException {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+
+}
